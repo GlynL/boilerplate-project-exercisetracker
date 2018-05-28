808e650b9eefb792f0f6c20cbc0216c107277368
Glyn L <glyn_92@hotmail.com>
2018-05-28 12:45:38 +0700

    refactored helpers

--- helpers/exercises.js
+++ helpers/exercises.js
@@ -2,16 +2,7 @@ const db = require("../models");
 
 exports.addExercise = function(req, res) {
   // create current date if not provided
-  if (!req.body.date) {
-    const date = new Date();
-    const year = date.getFullYear();
-    let month = date.getMonth() + 1; /* returns 0 -> 11 for some reason */
-    if (month < 10) month = `0${month}`; /* ensure standard formatting for consitency & search accuracy */
-    let day = date.getDate();
-    if (day < 10) day = `0${day}`;
-    const formattedDate = `${year}-${month}-${day}`;
-    req.body.date = formattedDate;
-  }
+  if (!req.body.date) req.body.date = createDate();
   // check all required fields are filled
   if (!req.body.userId || !req.body.description || !req.body.duration)
     return res.send("missing field");
@@ -23,8 +14,8 @@ exports.getExercises = function(req, res) {
   const id = req.query.userId;
   const limit = Number(req.query.limit) || 0;
   const fromDate = req.query.from || "0000-00-00";
-  console.log(req.query.from);
   const toDate = req.query.to || "9999-12-31";
+  // query db for matching results
   db.Exercise.find({ userId: id })
     .where("date")
     .gte(fromDate)
@@ -36,10 +27,13 @@ exports.getExercises = function(req, res) {
     .catch(err => res.send(err));
 };
 
-/* 
-TODO:
-* move stuff into middlewares?
-  - what are middlewares exactly
-* tidy code
-* add 0 to automatic date
-*/
+function createDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1; /* returns 0 -> 11 for some reason */
+  if (month < 10)
+    month = `0${month}`; /* ensure standard formatting for consitency & search accuracy */
+  let day = date.getDate();
+  if (day < 10) day = `0${day}`;
+  return `${year}-${month}-${day}`;
+}
